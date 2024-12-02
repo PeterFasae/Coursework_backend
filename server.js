25dfade222b91daa88186689f03de69be99a6157
Peter Fasae <pfasae@gmail.com>
2024-12-02 06:31:49 +0000

    added the main variables for express

--- server.js
+++ server.js
@@ -1,2 +1,9 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const morgan = require('morgan');
+const fs = require('fs');
+const { MongoClient } = require('mongodb');
 
 
+const app = express();
