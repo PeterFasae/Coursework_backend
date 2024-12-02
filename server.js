95386730f987508f090e4dc17c1c98460527889b
Peter Fasae <pfasae@gmail.com>
2024-12-02 06:32:24 +0000

    connection to atlas

--- server.js
+++ server.js
@@ -7,3 +7,7 @@ const { MongoClient } = require('mongodb');
 
 
 const app = express();
+
+// MongoDB Atlas connection
+const MONGO_URI = 'mongodb+srv://peter_f:ayomide%402324@backend-cluster.4v1jr.mongodb.net/'; //  MongoDB Atlas URI
+const client = new MongoClient(MONGO_URI);
