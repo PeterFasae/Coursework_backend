21f6a4cd450a08d2ee64036601df662465ecb45e
Peter Fasae <pfasae@gmail.com>
2024-12-02 06:34:04 +0000

    database connection setup

--- server.js
+++ server.js
@@ -11,3 +11,19 @@ const app = express();
 // MongoDB Atlas connection
 const MONGO_URI = 'mongodb+srv://peter_f:ayomide%402324@backend-cluster.4v1jr.mongodb.net/'; //  MongoDB Atlas URI
 const client = new MongoClient(MONGO_URI);
+
+let productsCollection;
+let ordersCollection;
+
+// Connect to MongoDB Atlas
+(async () => {
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB Atlas');
+    const database = client.db('webstore'); //  Database name
+    productsCollection = database.collection('products'); //  collection name
+    ordersCollection = database.collection('orders'); //  collection name
+  } catch (err) {
+    console.error('Error connecting to MongoDB Atlas:', err);
+  }
+})();
